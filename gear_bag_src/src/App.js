6e635098416b05e630ef7e9d2a967a106d236f80
emmalopezcode <55823474+emmalopezcode@users.noreply.github.com>
2020-11-03 12:42:41 -0600

    fiddling

--- gear_bag_src/src/App.js
+++ gear_bag_src/src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Paper } from "@material-ui/core";
+import { GearGrid } from "./GearGrid"
+import $ from 'jquery';
+
+// const useStyles = makeStyles((theme) => ({
+
+//   grid: {
+//     width : '100%',
+//     margin : '0px'
+//   },
+
+//   paper: {
+//     padding: theme.spacing(2)
+
+//   }
+
+// }));
+
+
+
+
+
+function GearItem() {
+  return <Grid item xs={12} md={4}>
+    <Paper className="test"> 10 </Paper>
+  </Grid>;
+}
+
 
 function App() {
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React...
-        </a>
-      </header>
+      <GearGrid></GearGrid>
     </div>
   );
 }
